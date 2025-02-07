f706d5eac83c988ed438c9086c0c962202b9d9fc
levimohle <levi.mohle@asml.com>
2025-02-07 11:27:35 +0100

    fixed device convert bug

--- src/models/support_functions/evaluation.py
+++ src/models/support_functions/evaluation.py
@@ -268,121 +268,120 @@ def rgb_to_gray(x):
      return x
 
 def class_reconstructs_2ch(self, x, reconstructs, plot_ids, fs=12):
+        x = rgb_to_gray(x.cpu())
 
-     x = rgb_to_gray(x)
+        ssim_orig_vs_reconstruct = []
+        for i, reconstruct in enumerate(reconstructs):
+            reconstructs[i] = rgb_to_gray(reconstruct).cpu()
 
-     ssim_orig_vs_reconstruct = []
-     for i, reconstruct in enumerate(reconstructs):
-          reconstructs[i] = rgb_to_gray(reconstruct)
+            # Calculate SSIM between original sample and all reconstructed labels
+            ssim_orig_vs_reconstruct.append(ssim_for_batch(x, reconstruct))
 
-          # Calculate SSIM between original sample and all reconstructed labels
-          ssim_orig_vs_reconstruct.append(ssim_for_batch(x, reconstruct))
+        ssim_l0_vs_l1 = ssim_for_batch(reconstructs[0], reconstructs[1])
 
-     ssim_l0_vs_l1 = ssim_for_batch(reconstructs[0], reconstructs[1])
+        # img = [self.min_max_normalize(x, dim=(2,3)).cpu(), self.min_max_normalize(reconstruct, dim=(2,3)).cpu(), error_idv, error_comb]
+        extent = [0,4,0,4]
+        for i in plot_ids:
+            fig = plt.figure(constrained_layout=False, figsize=(15,17))
+            gs = GridSpec(4, 4, figure=fig, width_ratios=[1.08,1,1.08,1.08], height_ratios=[1,1,1,1], hspace=0.2, wspace=0.2)
+            
+            # RGB images
+            # Span whole column
+            ax1 = fig.add_subplot(gs[0:2,0])
+            ax6 = fig.add_subplot(gs[0:2,3])
 
-     # img = [self.min_max_normalize(x, dim=(2,3)).cpu(), self.min_max_normalize(reconstruct, dim=(2,3)).cpu(), error_idv, error_comb]
-     extent = [0,4,0,4]
-     for i in plot_ids:
-        fig = plt.figure(constrained_layout=False, figsize=(15,17))
-        gs = GridSpec(4, 4, figure=fig, width_ratios=[1.08,1,1.08,1.08], height_ratios=[1,1,1,1], hspace=0.2, wspace=0.2)
-        
-        # RGB images
-        # Span whole column
-        ax1 = fig.add_subplot(gs[0:2,0])
-        ax6 = fig.add_subplot(gs[0:2,3])
+            # Regular grid
+            ax2 = fig.add_subplot(gs[0,1])
+            ax3 = fig.add_subplot(gs[1,1])
+            ax4 = fig.add_subplot(gs[0,2])
+            ax5 = fig.add_subplot(gs[1,2])
 
-        # Regular grid
-        ax2 = fig.add_subplot(gs[0,1])
-        ax3 = fig.add_subplot(gs[1,1])
-        ax4 = fig.add_subplot(gs[0,2])
-        ax5 = fig.add_subplot(gs[1,2])
+            # Height images
+            # Span whole column
+            ax7 = fig.add_subplot(gs[2:4,0])
+            ax12 = fig.add_subplot(gs[2:4,3])
 
-        # Height images
-        # Span whole column
-        ax7 = fig.add_subplot(gs[2:4,0])
-        ax12 = fig.add_subplot(gs[2:4,3])
+            # Regular grid
+            ax8  = fig.add_subplot(gs[2,1])
+            ax9  = fig.add_subplot(gs[3,1])
+            ax10 = fig.add_subplot(gs[2,2])
+            ax11 = fig.add_subplot(gs[3,2])
 
-        # Regular grid
-        ax8  = fig.add_subplot(gs[2,1])
-        ax9  = fig.add_subplot(gs[3,1])
-        ax10 = fig.add_subplot(gs[2,2])
-        ax11 = fig.add_subplot(gs[3,2])
+            # Plot rgb
+            im1 = ax1.imshow(x[i,0], extent=extent, vmin=0, vmax=1)
+            ax1.set_yticks([0,1,2,3,4])
+            # ax1.tick_params(axis='both', which='both', labelbottom=False, labelleft=True)
+            ax1.set_title("Original sample", fontsize =fs)
+            ax1.set_ylabel("Y [mm]")
+            ax1.set_xlabel("X [mm]")
+            ax1.text(-0.3, 0.5, "Gray-scale", fontsize= fs*2, rotation=90, va="center", ha="center", transform=ax1.transAxes)
+            divider = make_axes_locatable(ax1)
+            cax1 = divider.append_axes("right", size="5%", pad=0.1)
+            plt.colorbar(im1, cax=cax1)
 
-        # Plot rgb
-        im1 = ax1.imshow(x[i,0], extent=extent, vmin=0, vmax=1)
-        ax1.set_yticks([0,1,2,3,4])
-        # ax1.tick_params(axis='both', which='both', labelbottom=False, labelleft=True)
-        ax1.set_title("Original sample", fontsize =fs)
-        ax1.set_ylabel("Y [mm]")
-        ax1.set_xlabel("X [mm]")
-        ax1.text(-0.3, 0.5, "Gray-scale", fontsize= fs*2, rotation=90, va="center", ha="center", transform=ax1.transAxes)
-        divider = make_axes_locatable(ax1)
-        cax1 = divider.append_axes("right", size="5%", pad=0.1)
-        plt.colorbar(im1, cax=cax1)
+            for j, ax in enumerate([ax2, ax3]):
+                ax.imshow(reconstructs[j][i,0], extent=extent, vmin=0, vmax=1)
+                ax.set_yticks([0,1,2,3,4])
+                ax.set_xlabel("X [mm]")
+                ax.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
+                ax.set_title(f"Reconstructed sample label {j}", fontsize =fs)
 
-        for j, ax in enumerate([ax2, ax3]):
-            ax.imshow(reconstructs[j][i,0], extent=extent, vmin=0, vmax=1)
-            ax.set_yticks([0,1,2,3,4])
-            ax.set_xlabel("X [mm]")
-            ax.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
-            ax.set_title(f"Reconstructed sample label {j}", fontsize =fs)
+            for j, ax in enumerate([ax4, ax5]):
+                im = ax.imshow(ssim_orig_vs_reconstruct[j][i,0], extent=extent, vmin=0, vmax=1)
+                ax.set_yticks([0,1,2,3,4])
+                ax.set_xlabel("X [mm]")
+                ax.set_ylabel("Y [mm]")
+                ax.set_title(f"SSIM label {j} recon vs orig", fontsize =fs)
+                divider = make_axes_locatable(ax)
+                cax = divider.append_axes("right", size="5%", pad=0.1)
+                plt.colorbar(im, cax=cax)
 
-        for j, ax in enumerate([ax4, ax5]):
-            im = ax.imshow(ssim_orig_vs_reconstruct[j][i,0], extent=extent, vmin=0, vmax=1)
-            ax.set_yticks([0,1,2,3,4])
-            ax.set_xlabel("X [mm]")
-            ax.set_ylabel("Y [mm]")
-            ax.set_title(f"SSIM label {j} recon vs orig", fontsize =fs)
-            divider = make_axes_locatable(ax)
-            cax = divider.append_axes("right", size="5%", pad=0.1)
-            plt.colorbar(im, cax=cax)
+            im6 = ax6.imshow(ssim_l0_vs_l1[i,0], extent=extent, vmin=0)
+            ax6.set_yticks([0,1,2,3,4])
+            ax6.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
+            ax6.set_xlabel("X [mm]")
+            ax6.set_title(f"SSIM label 0 vs label 1 recon", fontsize =fs)
+            divider = make_axes_locatable(ax6)
+            cax6 = divider.append_axes("right", size="5%", pad=0.1)
+            plt.colorbar(im6, cax=cax6)
 
-        im6 = ax6.imshow(ssim_l0_vs_l1[i,0], extent=extent, vmin=0)
-        ax6.set_yticks([0,1,2,3,4])
-        ax6.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
-        ax6.set_xlabel("X [mm]")
-        ax6.set_title(f"SSIM label 0 vs label 1 recon", fontsize =fs)
-        divider = make_axes_locatable(ax6)
-        cax6 = divider.append_axes("right", size="5%", pad=0.1)
-        plt.colorbar(im6, cax=cax6)
+            # Plot height
+            im7 = ax7.imshow(x[i,1], extent=extent, vmin=0, vmax=1)
+            ax7.set_yticks([0,1,2,3,4])
+            ax7.set_title("Original sample", fontsize =fs)
+            ax7.set_ylabel("Y [mm]")
+            ax7.set_xlabel("X [mm]")
+            ax7.text(-0.3, 0.5, "Height", fontsize= fs*2, rotation=90, va="center", ha="center", transform=ax7.transAxes)
+            divider = make_axes_locatable(ax7)
+            cax7 = divider.append_axes("right", size="5%", pad=0.1)
+            plt.colorbar(im7, cax=cax7)
 
-        # Plot height
-        im7 = ax7.imshow(x[i,1], extent=extent, vmin=0, vmax=1)
-        ax7.set_yticks([0,1,2,3,4])
-        ax7.set_title("Original sample", fontsize =fs)
-        ax7.set_ylabel("Y [mm]")
-        ax7.set_xlabel("X [mm]")
-        ax7.text(-0.3, 0.5, "Height", fontsize= fs*2, rotation=90, va="center", ha="center", transform=ax7.transAxes)
-        divider = make_axes_locatable(ax7)
-        cax7 = divider.append_axes("right", size="5%", pad=0.1)
-        plt.colorbar(im7, cax=cax7)
+            for j, ax in enumerate([ax8, ax9]):
+                ax.imshow(reconstructs[j][i,1], extent=extent, vmin=0, vmax=1)
+                ax.set_yticks([0,1,2,3,4])
+                ax.set_xlabel("X [mm]")
+                ax.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
+                ax.set_title(f"Reconstructed sample label {j}", fontsize =fs)
 
-        for j, ax in enumerate([ax8, ax9]):
-            ax.imshow(reconstructs[j][i,1], extent=extent, vmin=0, vmax=1)
-            ax.set_yticks([0,1,2,3,4])
-            ax.set_xlabel("X [mm]")
-            ax.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
-            ax.set_title(f"Reconstructed sample label {j}", fontsize =fs)
+            for j, ax in enumerate([ax10, ax11]):
+                im = ax.imshow(ssim_orig_vs_reconstruct[j][i,1], extent=extent, vmin=0, vmax=1)
+                ax.set_yticks([0,1,2,3,4])
+                ax.set_xlabel("X [mm]")
+                ax.set_ylabel("Y [mm]")
+                ax.set_title(f"SSIM label {j} recon vs orig", fontsize =fs)
+                divider = make_axes_locatable(ax)
+                cax = divider.append_axes("right", size="5%", pad=0.1)
+                plt.colorbar(im, cax=cax)
 
-        for j, ax in enumerate([ax10, ax11]):
-            im = ax.imshow(ssim_orig_vs_reconstruct[j][i,1], extent=extent, vmin=0, vmax=1)
-            ax.set_yticks([0,1,2,3,4])
-            ax.set_xlabel("X [mm]")
-            ax.set_ylabel("Y [mm]")
-            ax.set_title(f"SSIM label {j} recon vs orig", fontsize =fs)
-            divider = make_axes_locatable(ax)
-            cax = divider.append_axes("right", size="5%", pad=0.1)
-            plt.colorbar(im, cax=cax)
+            im12 = ax12.imshow(ssim_l0_vs_l1[i,1], extent=extent, vmin=0)
+            ax12.set_yticks([0,1,2,3,4])
+            ax12.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
+            ax12.set_xlabel("X [mm]")
+            ax12.set_title(f"SSIM label 0 vs label 1 recon", fontsize =fs)
+            divider = make_axes_locatable(ax12)
+            cax12 = divider.append_axes("right", size="5%", pad=0.1)
+            plt.colorbar(im12, cax=cax12)
 
-        im12 = ax12.imshow(ssim_l0_vs_l1[i,1], extent=extent, vmin=0)
-        ax12.set_yticks([0,1,2,3,4])
-        ax12.tick_params(axis='both', which='both', labelbottom=True, labelleft=False)
-        ax12.set_xlabel("X [mm]")
-        ax12.set_title(f"SSIM label 0 vs label 1 recon", fontsize =fs)
-        divider = make_axes_locatable(ax12)
-        cax12 = divider.append_axes("right", size="5%", pad=0.1)
-        plt.colorbar(im12, cax=cax12)
-
-        plt_dir = os.path.join(self.image_dir, f"{self.current_epoch}_reconstructs_{i}.png")
-        fig.savefig(plt_dir)
-        plt.close()
+            plt_dir = os.path.join(self.image_dir, f"{self.current_epoch}_reconstructs_{i}.png")
+            fig.savefig(plt_dir)
+            plt.close()
