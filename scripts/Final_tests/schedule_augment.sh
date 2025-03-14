c18d2b8763d264585d92fda8eeb7fa36c03a0d35
levimohle <levi.mohle@asml.com>
2025-03-14 10:34:48 +0100

    minor changes to schedule scripts

--- scripts/Final_tests/schedule_augment.sh
+++ scripts/Final_tests/schedule_augment.sh
@@ -3,12 +3,12 @@
 # Run from root folder with: bash scripts/schedule.sh
 
 # Runs on models trained without augmented data
-python src/eval.py +experiment=impasto_FM2 trainer=gpu data.variant=Enc_512x512 data.crack=realBI model.FM_param.reconstruct=.4 model.FM_param.plot_ids=[2,5,10,12,13,15] ckpt_path=/data/storage_models_crack_detection/Trained_models/2025-03-13_00-23-08_FM_new/checkpoints/epoch_064.ckpt
+python src/eval.py +experiment=impasto_FM2 trainer=gpu data.variant=Enc_512x512 data.crack=realBI,synthetic model.FM_param.reconstruct=.3 model.FM_param.plot_ids=[2,5,10,12,13,15] ckpt_path=/data/storage_models_crack_detection/Trained_models/2025-03-13_00-23-08_FM_new/checkpoints/epoch_064.ckpt --multirun
 
-python src/eval.py +experiment=impasto_FM2 trainer=gpu data.variant=Enc_512x512 data.crack=realBI model.FM_param.reconstruct=.4 model.FM_param.plot_ids=[2,5,10,12,13,15] ckpt_path=/data/storage_models_crack_detection/Trained_models/2025-03-12_16-41-10_FM_aug_new/checkpoints/epoch_068.ckpt 
 ##############
-# python src/eval.py +experiment=impasto_cddpm2 trainer=gpu data.variant=Enc_512x512 data.crack=realBI model.DDPM_param.reconstruct=.4 model.DDPM_param.plot_ids=[2,5,10,12,13,15] ckpt_path= 
+python src/eval.py +experiment=impasto_cddpm2 trainer=gpu data.variant=Enc_512x512 data.crack=realBI,synthetic model.DDPM_param.use_cond=false model.DDPM_param.reconstruct=.1 model.DDPM_param.plot_ids=[2,5,10,12,13,15] ckpt_path=/data/storage_models_crack_detection/Trained_models/2025-03-12_21-12-29_DDPM_new/checkpoints/epoch_023.ckpt --multirun
 
+python src/eval.py +experiment=impasto_cddpm2 trainer=gpu data.variant=Enc_512x512 data.crack=realBI,synthetic model.DDPM_param.use_cond=true model.DDPM_param.reconstruct=.4 model.DDPM_param.plot_ids=[2,5,10,12,13,15] ckpt_path=/data/storage_models_crack_detection/Trained_models/2025-03-12_21-12-29_DDPM_new/checkpoints/epoch_023.ckpt --multirun
 
 ################
 # python src/eval.py +experiment=impasto_cddpm2 trainer=gpu data.variant=Enc_512x512 model.DDPM_param.use_cond=true data.crack=realBI model.DDPM_param.reconstruct=.4 model.DDPM_param.plot_ids=[2,5,10,12,13,15] ckpt_path=/data/storage_models_crack_detection/Trained_models/2025-03-12_21-12-29_DDPM_new/checkpoints/epoch_023.ckpt 
